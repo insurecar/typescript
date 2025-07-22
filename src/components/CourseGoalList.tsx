eb10b28b43eccde5c128657b4fb0518500493430
Rostyslav Shyian <rostyslavshyian@Rostyslavs-MacBook-Air-6225.local>
2025-07-22 16:06:35 +0200

    finished 49

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -10,14 +10,16 @@ type CourseGoalListProps = {
 const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
   if (goals.length === 0) {
     return (
-      <InfoBox mode="hint">You have no course yet. Start adding some!</InfoBox>
+      <InfoBox severity="low" mode="hint">
+        You have no course yet. Start adding some!
+      </InfoBox>
     );
   }
   let warningBox: ReactNode;
 
   if (goals.length >= 4) {
     warningBox = (
-      <InfoBox mode="warning">
+      <InfoBox severity="high" mode="warning">
         You are collecting a lot of goals. Don't put too much on your plate!
       </InfoBox>
     );
--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -2,16 +2,17 @@ import { ReactNode } from "react";
 
 type InfoBoxProps = {
   mode: "hint" | "warning";
+  severity?: "low" | "medium" | "high";
   children: ReactNode;
 };
 
-export const InfoBox = ({ mode, children }: InfoBoxProps) => {
+export const InfoBox = ({ mode, severity, children }: InfoBoxProps) => {
   if (mode === "hint") {
     return <aside className="infobox infobox-hint">{children}</aside>;
   }
 
   return (
-    <aside className="infobox infobox-warning warning--medium">
+    <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
       <p>{children}</p>
     </aside>
